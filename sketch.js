ba737851447f6cb3cf19119648566546cb08c2d2
bmclach <77411753+bmclach@users.noreply.github.com>
2021-03-30 14:43:59 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 function setup() {
   
-  createCanvas(600, 300);
-  
+  var title = createCanvas(600, 100);
+  title.parent("title");
 }
 
 function draw() {
+  textAlign(CENTER, CENTER);
+  colorMode(RGB, 600, 100, 255);
   background(mouseX, mouseY, 0);
   noFill();
   stroke(255);
@@ -12,4 +14,8 @@ function draw() {
   if (random(600) < mouseX/2) {
     ellipse(random(width), random(height), 40, 40);
   }
+  fill(22, 43, 11);
+  strokeWeight(2);
+  textSize(50);
+  text("All About Droogan", 300, 50);
 }
